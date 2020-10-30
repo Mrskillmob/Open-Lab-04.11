4aa7bd87e67d4ed221451f87ab1062b7bbae1302
Martin Kubica <martinko29.km@gmail.com>
2020-10-30 16:25:23 +0100

    +

--- Open-Lab-04.11/StringTools.cs
+++ Open-Lab-04.11/StringTools.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace Open_Lab_04._11
 {
@@ -6,7 +10,17 @@ namespace Open_Lab_04._11
     {
         public string AlphabetSoup(string str)
         {
-            throw new NotImplementedException();
+            
+   
+            char[] nvm = new char[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                nvm[i] = str[i];
+            }
+            Array.Sort(nvm);
+            string b = string.Join("", nvm);
+
+            return b;
         }
     }
 }
